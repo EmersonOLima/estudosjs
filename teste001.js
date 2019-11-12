031735aae550b3df72805b390b3be6310ef49393
Emerson Alves Lima <emersonsintemas@gmail.com>
2019-11-12 15:58:34 -0300

    alteração no counter

--- teste001.js
+++ teste001.js
@@ -1,5 +1,5 @@
 var result = 1;
-for (var counter = 0; counter <10; counter = counter + 1){
+for (var counter = 0; counter <12; counter = counter + 1){
     result = result * 2
 }
 console.log(result)
